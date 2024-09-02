37acdb180da43c2c35e5c310fba77015e87b176f
AntGe <your_public_email@example.com>
2024-09-02 18:15:30 +0800

    feat:4.在 JSX 中通过大括号使用 JavaScript

---
+++ 02.描述UI/4.在 JSX 中通过大括号使用 JavaScript/use_jsx/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Gallery from './Gallery'
+
+// 定义一个名为APP的函数组件
+function App() {
+  return (
+    <div className="App">
+      {/* Gallery的函数组件 */}
+      <Gallery />
+    </div>
+  );
+}
+// export default 导出组件
+export default App;
